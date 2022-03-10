ad2eae12fe73fa7f1fd3d5563792584db7851276
littleKingkong-source <2674123004@qq.com>
2022-03-10 22:48:06 +0800

    first commit

---
+++ src/test/java/com/littlekingkong/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlekingkong.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
